d5481a77b2e65983a66b872c775ba43cb98cb0d2
Mark A. Caprio <mcaprio-nd-edu>
2017-12-10 13:37:09 -0500

    module.mk: cleanup

--- module.mk
+++ module.mk
@@ -10,8 +10,9 @@ module_units_cpp-h += lsjt_scheme lsjt_operator jjjt_scheme jjjt_operator
 module_units_cpp-h += jjjpn_scheme jjjpn_operator nlj_orbital nlj_operator
 module_units_cpp-h += m_scheme
 # module_units_f := 
+module_programs_cpp := 
 ifdef BASIS_ENABLE_UNIT_TEST
-  module_programs_cpp := degenerate_test hypersector_test
+  module_programs_cpp += degenerate_test hypersector_test
   module_programs_cpp += lsjt_scheme_test lsjt_operator_test jjjt_scheme_test
   module_programs_cpp += jjjpn_scheme_test jjjpn_operator_test
   module_programs_cpp += nlj_orbital_test m_scheme_test
